689dad51bece4f0cf00a40be8b98965d550f81c1
Lisa Williams <lisaismyname0@gmail.com>
2022-07-18 15:51:21 -0400

    buttons logging event target

--- src/nav_button.js
+++ src/nav_button.js
@@ -1,10 +1,14 @@
+const handleClick = (event) => {
+    console.log(event.target)
+}
+
 export default function NavButton(){
     return (
         <div>
-        <button>Go Back</button>
-        <button> Next </button>
-        <button> Start Over</button>
-        <button> Jump to Slide *INPUT BOX*</button>
+        <button onClick= {(e) => handleClick(e)}>Go Back</button>
+        <button onClick= {(e) => handleClick(e)}> Next </button>
+        <button onClick= {(e) => handleClick(e)}> Start Over</button>
+        <button onClick= {(e) => handleClick(e)}> Jump to Slide *INPUT BOX*</button>
         </div>
     )
 }
