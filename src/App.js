cf18e02fc071bf8bf41994ea9aeb1f98c6cdc908
Lisa Williams <lisaismyname0@gmail.com>
2022-07-19 12:31:17 -0400

    removed nav_button component, moved buttons to slideshow.js, clicking start over also redirects to slide1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css'
 import SlideShow from './slideshow'
-import NavButton from './nav_button'
 import React, {useState, useEffect} from 'react';
 
 export default function App() {
 return(
   <div>
   <SlideShow/>
-  <NavButton/>
   </div>
 )}
--- src/slideshow.js
+++ src/slideshow.js
@@ -41,61 +41,30 @@ export default function SlideShow(films){
             <Slide film={film1}/>
         )
     }
+
+
+const handleClick = (event) => { console.log(event.target)
+    }
+
     return(<div>
         <h1>Ghibli React Project</h1>
-        <div id='screen'>
-        {start ? 
-    (<div>
-    <h2>Click The Button to begin the Presentation</h2>
-        <button onClick={() => startPresentation()}> Start Presentation Function</button></div>): (
-        data.map((film, index) => (
-          <Slide film ={data} key ={index}/> ))
+        <div id='screenStart'>
+            <div>
+                <button onClick={() => startPresentation()}> Start Presentation Function</button>
+            </div>
 
-        )}
         {currentFilm ? (<Slide film={currentFilm} key={currentFilm.key}/>) : ("")}
 
-        {!data ? <Slide films={data}/> :<p> Loading...</p>}
+        {!data ? <Slide films={data}/> : ("")}
+
+
+<button onClick= {(e) => handleClick(e)}>Go Back</button>
+<button onClick= {(e) => handleClick(e)}> Next </button>
+<button onClick= {(e) => startPresentation(e)}> Start Over</button>
+<button onClick= {(e) => handleClick(e)}> Jump to Slide *INPUT BOX*</button>
+
+
+        
 
     </div></div>)
 }
-//     const startPresentation = (film, index) => {
-//         console.log("Lets Begin", film)
-//         setStart()
-//         console.log(currentFilm)
-//         handleCurrentFilm(film)
-//     }
-
-//     const handleCurrentFilm = (film) =>{
-//         console.log('Selected Film ', film)
-//         setCurrentFilm(film)
-//         console.log(`this is the ${currentFilm}`)
-        
-//     }
-//     return(<div>
-//     <h1>Ghibli React Project</h1>
-//     <div id='screen'>
-
-//     {start ? 
-//     (<div>
-//     <h2>Click The Button to begin the Presentation</h2>
-//         <button onClick={(film, index) => startPresentation(film)}> Start Presentation Function</button></div>): (
-//         films.map((film, index) => (
-//           <Slide film ={film} key ={index}/>
-//         ))
-        
-//     )}
-
-//     {/* {currentFilm ? (<Slide film={currentFilm} key={currentFilm.key}/>) : ("")} */}
-    
-//     {/* { currentFilm ? (
-//         <Slide film={currentFilm} key={currentFilm.key}/>) : (
-//         data.map((film, index) => (
-//             <div className= "film" onClick={() => handleCurrentFilm(film)}>
-//         <Slide film ={film} key ={index}/>
-//         </div>
-//         ))
-//     )} */}
-
-//     </div></div>)
-// }
-
