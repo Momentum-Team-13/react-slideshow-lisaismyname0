6f25f7c50d1c7b70c6c3623796b7a4efb6a19710
Lisa Williams <lisaismyname0@gmail.com>
2022-07-18 20:53:17 -0400

    moved api fetch request to slide compononent

--- src/App.js
+++ src/App.js
@@ -4,33 +4,8 @@ import NavButton from './nav_button'
 import { useEffect, useState } from "react";
 
 function App() {
-  const [data, setData]=useState([]);
-  const getData=()=>{
-      fetch("https://ghibliapi.herokuapp.com/films/"
-      ,{
-          headers : {
-              'Content-Type':'application/json',
-              'Accept': 'application/json'
-          }
-      }
-      )
-      .then(function(response){
-          console.log(response)
-          return response.json();
-      })
-      .then(function(myJson){
-          console.log(myJson);
-          setData(myJson)
-      });
-  }
-  
-  useEffect(() =>{
-      getData()
-  },[])
-  return(
-    <div className="App">
-      <h1>Studio Ghibli Slideshow</h1>
-{data.map((film) => <p>{film.title}</p>)}
+return(
+  <div>
       <SlideShow/>
       <NavButton/>
     </div>)
--- src/slide.js
+++ src/slide.js
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react";
 
 export default function Slide(){
+    const [data, setData]=useState([]);
+    const getData=()=>{
+        fetch("https://ghibliapi.herokuapp.com/films/"
+        ,{
+            headers : {
+                'Content-Type':'application/json',
+                'Accept': 'application/json'
+            }
+        }
+        )
+        .then(function(response){
+            console.log(response)
+            return response.json();
+        })
+        .then(function(myJson){
+            console.log(myJson);
+            setData(myJson)
+        });
+    }
+    
+    useEffect(() =>{
+        getData()
+    },[])
     return(
-        <div id= 'screen'>
-            <p>Slide Info</p>
-        </div>
-    )
-}
+      <div id="screen">
+  {data.map((film, index) => <p key ={index}>{index} {film.title}</p>)}
+      </div>)
+  }
